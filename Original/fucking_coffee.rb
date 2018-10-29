21bd81a17c65c6526527d524d928638f060d3d74
paulmathai01 <paulmathai01@gmail.com>
2018-10-29 11:39:05 +0530

    Initial fork commit

---
+++ Original/fucking_coffee.rb
@@ -0,0 +1,18 @@
+
+exit unless `who -q`.include? ENV['USER']
+
+require 'net/telnet'
+
+coffee_machine_ip = '10.10.42.42'
+password = '1234'
+password_prompt = 'Password: '
+delay_before_brew = 17
+delay = 24
+
+sleep delay_before_brew
+con = Net::Telnet.new('Host' => coffee_machine_ip)
+con.cmd('String' => password, 'Match' => /#{password_prompt}/)
+con.cmd('sys brew')
+sleep delay
+con.cmd('sys pour')
+con.close
